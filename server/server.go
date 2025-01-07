758428a511ef65c0c17591add3446d20f346c8ac
VanPC <safivan45@gmail.com>
2025-01-07 13:36:14 +0300

    new functionality to server.go

--- server/server.go
+++ server/server.go
@@ -3,15 +3,18 @@ package server
 import (
 	"fmt"
 	"net"
+	"strings"
 	"sync"
 )
 
-type Connections []net.Conn
+// net.Conn / IP or Username.
+type Connections map[net.Conn]any
 
 type ServerManager struct {
 	Listener  net.Listener
 	ConnList  Connections
 	ConnMutex sync.Mutex
+	ChManager *ChannelManager
 	//ConnManager ConnectionManager
 }
 
@@ -19,7 +22,7 @@ type ServerManagerInterface interface {
 	StartServer() error
 	CloseServer() error
 	AcceptConnection() error
-	HandleConnection(conn net.Conn) error
+	HandleConnection(conn Connections) error
 }
 
 func (s *ServerManager) CreateServer() error {
@@ -34,7 +37,7 @@ func (s *ServerManager) CreateServer() error {
 
 func (s *ServerManager) CloseServer() error {
 	s.ConnMutex.Lock()
-	for _, conn := range s.ConnList {
+	for conn := range s.ConnList {
 		conn.Close()
 	}
 	s.ConnList = nil
@@ -59,23 +62,60 @@ func (s *ServerManager) AcceptConnection() error {
 }
 
 func (s *ServerManager) HandleConnection(conn net.Conn) error {
-	defer conn.Close()
+	defer func() {
+		conn.Close()
+		fmt.Printf("Connection closed: %v\n", conn.RemoteAddr())
+		s.ConnMutex.Lock()
+		delete(s.ConnList, conn)
+		s.ConnMutex.Unlock()
+	}()
+
 	fmt.Printf("New connection from %v", conn.RemoteAddr())
 
 	s.ConnMutex.Lock()
-	s.ConnList = append(s.ConnList, conn)
+	s.ConnList[conn] = conn.RemoteAddr().String()
 	s.ConnMutex.Unlock()
 
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, 256)
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
 			fmt.Printf("Connection err form %v: %v\n", conn.RemoteAddr(), err)
 			break
 		}
-		recievedData := string(buffer[:n])
-		//change logic
-		fmt.Print(recievedData)
+
+		recievedData := strings.TrimSpace(string(buffer[:n]))
+		if len(recievedData) == 0 {
+			continue
+		}
+
+		switch {
+
+		// Case to change username.
+		case strings.HasPrefix(recievedData, "/name"):
+			newName := strings.TrimSpace(strings.TrimPrefix(recievedData, "/name"))
+			if newName == "" {
+				conn.Write([]byte("ERROR Invalid username.\n"))
+				continue
+			}
+			s.ConnMutex.Lock()
+			s.ConnList[conn] = newName
+			s.ConnMutex.Unlock()
+			conn.Write([]byte(fmt.Sprintf("SUCCESS username changed to: %v", newName)))
+
+		// Case to show channels.
+		case strings.HasPrefix(recievedData, "/showch"):
+			s.ConnMutex.Lock()
+			list := ""
+			for channelName := range s.ChManager.List {
+				list += (channelName + " ")
+			}
+			s.ConnMutex.Unlock()
+			conn.Write([]byte("Available channels: " + list + "\n"))
+
+		default:
+			conn.Write([]byte("Unrecognized command.\n"))
+		}
 	}
 	return nil
 }
