c4294b8ba9ac12a62e17b76ce2ef2c462c3cf208
VanPC <safivan45@gmail.com>
2025-01-07 12:26:33 +0300

    fixed handleconn func in server.go

--- server/server.go
+++ server/server.go
@@ -3,13 +3,15 @@ package server
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-type Connections []*net.Conn
+type Connections []net.Conn
 
 type ServerManager struct {
-	Listener net.Listener
-	ConnList Connections
+	Listener  net.Listener
+	ConnList  Connections
+	ConnMutex sync.Mutex
 	//ConnManager ConnectionManager
 }
 
@@ -31,11 +33,17 @@ func (s *ServerManager) CreateServer() error {
 }
 
 func (s *ServerManager) CloseServer() error {
+	s.ConnMutex.Lock()
+	for _, conn := range s.ConnList {
+		conn.Close()
+	}
+	s.ConnList = nil
+	s.ConnMutex.Unlock()
 	err := s.Listener.Close()
 	if err != nil {
 		return err
 	}
-	fmt.Println("Server succesfully created!")
+	fmt.Println("Server succesfully closed!")
 	return nil
 }
 
@@ -43,14 +51,31 @@ func (s *ServerManager) AcceptConnection() error {
 	for {
 		conn, err := s.Listener.Accept()
 		if err != nil {
-			fmt.Printf("Error while connection: %v", err)
+			fmt.Printf("Error while connection: %v\n", err)
 			continue
 		}
-		go s.HandleConnection(&conn)
+		go s.HandleConnection(conn)
 	}
 }
 
-func (s *ServerManager) HandleConnection(conn *net.Conn) error {
+func (s *ServerManager) HandleConnection(conn net.Conn) error {
+	defer conn.Close()
+	fmt.Printf("New connection from %v", conn.RemoteAddr())
+
+	s.ConnMutex.Lock()
 	s.ConnList = append(s.ConnList, conn)
+	s.ConnMutex.Unlock()
+
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Printf("Connection err form %v: %v\n", conn.RemoteAddr(), err)
+			break
+		}
+		recievedData := string(buffer[:n])
+		//change logic
+		fmt.Print(recievedData)
+	}
 	return nil
 }
